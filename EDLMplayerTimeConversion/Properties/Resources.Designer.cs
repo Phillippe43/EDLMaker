4cf6e97369a3f680266779bb16c68016aa7392ca
Phillippe43 <ALouviere@MIS-29.CLERK-EBR.com>
2015-04-02 18:21:01 -0500

    Delete Image

--- EDLMplayerTimeConversion/Properties/Resources.Designer.cs
+++ EDLMplayerTimeConversion/Properties/Resources.Designer.cs
@@ -93,16 +93,6 @@ namespace EDLMplayerTimeConversion.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap SuperDAD2 {
-            get {
-                object obj = ResourceManager.GetObject("SuperDAD2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap time {
             get {
                 object obj = ResourceManager.GetObject("time", resourceCulture);
